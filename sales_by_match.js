d4a835cd0a083956627acc359801cd2cc62d83ab
Jared Siemen <siemenjm@gmail.com>
2023-01-09 14:48:32 -0500

    Initial commit

---
+++ sales_by_match.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+  // Write your code here
+}
