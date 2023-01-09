cc7908df2ae05dc6d6a7b261d53b0c4242834b6b
Jared Siemen <siemenjm@gmail.com>
2023-01-09 15:02:14 -0500

    Initial commit

---
+++ counting_valleys.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+  // Write your code here
+}
