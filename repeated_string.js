f0bfbdb96c46f3e13b98ffe840844ea4b3d0b6bd
Jared Siemen <siemenjm@gmail.com>
2023-01-09 15:33:16 -0500

    Count a frequency in repeated string

--- repeated_string.js
+++ repeated_string.js
@@ -8,5 +8,32 @@
  */
 
 function repeatedString(s, n) {
-  // Write your code here
+  // find how times 'a' occurs in s
+  // find how many times s is repeated in a string n letters long
+  // multiply those two values
+  // take care of tail/scraps
+
+  let sCounter = Math.floor(n / s.length);
+  let sLeftovers = n % s.length;
+
+  let aCounter = 0;
+  for (let i = 0; i < s.length && i < n; i++) {
+    if (s[i] === 'a') {
+      aCounter++;
+    }
+  }
+
+  aCounter *= sCounter;
+
+  for (let i = 0; i < sLeftovers; i++) {
+    if (s[i] === 'a') {
+      aCounter++;
+    }
+  }
+
+  return aCounter;
 }
+
+console.log(repeatedString('abcac', 10)); // => 4
+console.log(repeatedString('aba', 10)); // => 7
+console.log(repeatedString('a', 1000000000000)); // => 7
