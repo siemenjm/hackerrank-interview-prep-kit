87aa44a321362bf58c48d87466d1f471595a4be3
Jared Siemen <siemenjm@gmail.com>
2023-01-09 15:34:52 -0500

    Initial commit

---
+++ array_left_rotation.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+function rotLeft(a, d) {
+  // Write your code here
+}
