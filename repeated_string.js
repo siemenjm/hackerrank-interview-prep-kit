78eb7ed2f2695a4a4b8485096114ea932ac342e0
Jared Siemen <siemenjm@gmail.com>
2023-01-09 15:18:50 -0500

    Initial commit

---
+++ repeated_string.js
@@ -0,0 +1,12 @@
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+function repeatedString(s, n) {
+  // Write your code here
+}
