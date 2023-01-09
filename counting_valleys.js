442927615105cac65fc62ab44ce55c04b33c753c
Jared Siemen <siemenjm@gmail.com>
2023-01-09 15:17:35 -0500

    Count valleys traversed

--- counting_valleys.js
+++ counting_valleys.js
@@ -8,5 +8,29 @@
  */
 
 function countingValleys(steps, path) {
-  // Write your code here
+  // track our elevation
+  // increment a valley counter every time we leave a valley
+  let valleysTraversed = 0;
+  let currentElevation = 0;
+  for (let i = 0; i < path.length; i++) {
+    if (steps < Math.abs(currentElevation)) {
+      return valleysTraversed;
+    }
+
+    if (path[i] === 'U' && currentElevation === -1) {
+      valleysTraversed++;
+      currentElevation++;
+    } else if (path[i] === 'U') {
+      currentElevation++;
+    } else if (path[i] === 'D') {
+      currentElevation--;
+    }
+
+    steps--;
+  }
+
+  return valleysTraversed;
 }
+
+console.log(countingValleys(8, ['D', 'D', 'U', 'U', 'U', 'U', 'D', 'D'])); // => 1
+console.log(countingValleys(8, ['U', 'D', 'D', 'D', 'U', 'D', 'U', 'U'])); // => 1
